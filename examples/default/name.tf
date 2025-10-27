6b1400bd2745a4959e647af765d93aaf9decbce5
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2025-10-27 14:16:28 -0500

    Resolving issue with namer module call

--- examples/default/name.tf
+++ examples/default/name.tf
@@ -1,5 +1,5 @@
 module "name" {
-  source  = "app.terraform.io/infoex/namer/azurerm"
+  source  = "app.terraform.io/infoex/namer/terraform"
   version = "0.0.1"
 
   contact     = "nobody@infoex.dev"
